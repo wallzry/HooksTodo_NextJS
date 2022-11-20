640d72037b76c664f6b02c9122069335ab81e7a0
wallzry <silasjonas@hotmail.de>
2022-11-20 11:23:46 +0100

    e

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react"
 import TodoList from "../components/TodoList"
 import TodoForm from "../components/TodoForm"
 import { v4 as uuidv4 } from "uuid"
+import Head from "next/head"
+import { AiOutlineOrderedList } from "react-icons/ai"
 
 const index = () => {
   const initialTodos = []
@@ -47,8 +49,28 @@ const index = () => {
         backgroundColor: "#fafafa",
       }}
     >
-      <div className="bg-main">
-        <p>Super Simple Todo App</p>
+      <Head>
+        <title>SILASF Todo App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="bg-main h-16 w-full relative">
+        <div className="absolute top-10 left-1/2 transform -translate-x-1/2 bg-white h-12 flex flex-col">
+          <h1 className="text-main px-1 text-3xl w-full text-center">SILASF</h1>
+          <div className="bg-main h-4 w-full flex items-center justify-center">
+            <p className="text-white w-full text-center text-xxs">
+              Digital Business Experts
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="mt-12">
+        <div className="text-2xl mx-auto text-center h-12 w-12 rounded bg-main flex justify-center items-center">
+          <AiOutlineOrderedList className="text-4xl shrink-0 grow-0 text-white" />
+          {/* <p className="text-white">Todo</p> */}
+        </div>
+        <p className="text-sm text-gray-400 italic text-center">
+          No registration. No installation. No data collected. Enjoy Simplicity.
+        </p>
       </div>
       <div className="flex flex-col justify-center mt-3">
         <TodoForm addTodo={addTodo} />
