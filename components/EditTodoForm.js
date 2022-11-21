c312d1275ef6a5d0e8200c346eaf2b2ae60ba584
wallzry <silasjonas@hotmail.de>
2022-11-21 15:14:44 +0100

    s

--- components/EditTodoForm.js
+++ components/EditTodoForm.js
@@ -1,4 +1,4 @@
-import { AiFillDelete, AiFillEdit } from "react-icons/ai"
+import { AiFillDelete, AiFillEdit, AiOutlineEnter } from "react-icons/ai"
 import useInputState from "./../hooks/useInputState"
 
 const EditTodoForm = ({ id, task, editTodo, toggleIsEditing }) => {
@@ -14,7 +14,7 @@ const EditTodoForm = ({ id, task, editTodo, toggleIsEditing }) => {
         toggleIsEditing()
       }}
     >
-      <div className="flex h-auto w-full items-center gap-2 mr-2">
+      <div className="flex h-auto w-full items-center gap-2 mr-2 relative">
         <input
           type="checkbox"
           className="accent-main h-6 w-6 cursor-pointer shrink-0 grow-0 border-0 border-gray-200"
@@ -22,11 +22,12 @@ const EditTodoForm = ({ id, task, editTodo, toggleIsEditing }) => {
         />
         <input
           type="text"
-          className="text-sm uppercase break-words w-full"
+          className="border rounded text-sm border-gray-200 p-2 uppercase break-words w-full"
           value={value}
           onChange={handleChange}
           autoFocus
         />
+        <AiOutlineEnter className="absolute top-1/2 right-0 transform -translate-x-1/2 -translate-y-1/2 text-black bg-gray-200 rounded text-2xl p-1" />
       </div>
       <div className="flex items-center gap-4">
         <div className="cursor-pointer text-gray-200">
